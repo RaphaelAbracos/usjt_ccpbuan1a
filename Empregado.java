02a1f82bbfce02e8cd4d5c0c399dcf606ea733a6
RaphaelAbracos <RaphaelAbracos@github.com>
2020-09-03 20:36:49 -0300

    Calcula Salario de acordo com o tipo

--- Empregado.java
+++ Empregado.java
@@ -1,17 +1,70 @@
 public class Empregado {
     private String nome;
     private int idade;
+    private int tipo;
+    private double salario;
+    private double comissao;
+    private double bonus;
+
+    public double calculaSalario() {
+        switch (tipo) {
+            case 1:
+                return salario;
+            case 2:
+                return salario + salario + comissao;
+            case 3:
+                return salario + bonus;
+            default:
+                return 0;
+        }
+    }
 
     public int getIdade() {
         return idade;
     }
+
     public String getNome() {
         return nome;
     }
+
     public void setIdade(int idade) {
         this.idade = idade;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+    public void setComissao(double comissao) {
+        this.comissao = comissao;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getBonus() {
+        return bonus;
+    }
+
+    public double getComissao() {
+        return comissao;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
 }
